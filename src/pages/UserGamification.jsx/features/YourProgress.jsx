91ced0fd2a9c202de579c3e03834718534cbe935
Kristi Rts <kristireci01@gmail.com>
2024-08-05 18:11:46 +0300

    aa

--- src/pages/UserGamification.jsx/features/YourProgress.jsx
+++ src/pages/UserGamification.jsx/features/YourProgress.jsx
@@ -9,9 +9,6 @@ import CloseButton from '../../../features/UI/Buttons/CloseButton';
 import MainButton from '../../../features/UI/Buttons/MainButton';
 
 import { getUserAchievements } from '../gamificationAsyncActions';
-import { gamificationActions } from '../userGamificationSlice';
-
-import { modalActions } from '../../../features/ModalRoot/modalSlice';
 
 import { translate } from '../../../utils/translations';
 
@@ -26,20 +23,20 @@ const YourProgress = () => {
 
     const lang = useSelector((state) => state.app.lang);
 
-    //const user = useSelector((state) => state.login.user);
     const selectedHero = useSelector((state) => state.gamification.selectedHero);
     const selectedHeroLevels = useSelector((state) => state.gamification.heroLevels);
     const currentUserLevel = useSelector((state) => state.gamification.currentLevel);
 
     const [activeLevel, setActiveLevel] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const controller = new AbortController();
         const signal = controller.signal;
 
-        dispatch(getUserAchievements(signal));
+        dispatch(getUserAchievements(signal)).then(() => setLoading(false));
 
-        return () => { };
+        return () => controller.abort();
     }, [dispatch]);
 
     useEffect(() => {
@@ -60,75 +57,93 @@ const YourProgress = () => {
         navigate(`/profile?${searchParams.toString()}`, { replace: true });
     };
 
+    // if (loading) {
+    //     return (
+    //         <div className={classes.Loading}>
+    //             <div className={classes.Spinner}></div>
+    //         </div>
+    //     );
+    // }
+
     return (
         <div className={classes.Modal}>
-
-            <div className={classes.ImageContainer}>
-                <img src={selectedHero.banner} loading="lazy" alt={selectedHero.name} />
-            </div>
-
-            <div className={classes.ModalContent}>
-                {selectedHero && Object.keys(selectedHero).length > 0 ? (
-                    <div className={classes.BackgroundContainer}>
-                        <div className={classes.TopContent}>
-                            <header>
-                                <div className={classes.Center}>
-                                    <h1 className={classes.Title}>
-                                        {translate('Your Progress')}
-                                    </h1>
-                                </div>
-                                <div className={classes.Right}>
-                                    <CloseButton timesIcon onClick={() => navigate(location.pathname)} />
-                                </div>
-                                <div className={classes.Center}>
-                                    <h1 className={classes.Hero}>
-                                        {translate(selectedHero.name + ' ' + selectedHero.subName)}
-                                    </h1>
-                                </div>
-                            </header>
-                        </div>
-                    </div>
-                ) : (
-                    <div className={classes.BackgroundContainer}>
-                        <div className={classes.TopContent}>
-                            <header>
-                                <div className={classes.Center}>
-                                    <h1 className={classes.Title} style={{ width: '100%' }}>
-                                        {translate('No hero has been selected.')}
-                                    </h1>
-                                </div>
-                                <div className={classes.Right}>
-                                    <CloseButton timesIcon onClick={() => navigate(location.pathname)} />
-                                </div>
-                            </header>
-                        </div>
-                    </div>
-                )}
-
-                <div className={classes.MainContent}>
-                    {selectedHero && Object.keys(selectedHero).length > 0 ? (
-                        <div className={classes.GridContainer}>
-                            {activeLevel != null &&
-                                <section className={classes.LevelUpSection}>
-                                    <Levels activeLevel={activeLevel} onChangeLevel={(level) => setActiveLevel(level)} />
-                                    <div className={classes.LevelUpMilestone}>
-                                        <Milestones activeLevel={activeLevel} progressBar />
-                                    </div>
-                                    <Rewards />
-                                </section>
-                            }
-
-                        </div>
-                    ) : (
-                        <div className={classes.GoToButton}>
-                            <MainButton color='primary' size='small' onClick={() => addParamsToUrl('heroes')}>
-                                {translate('Select a Hero')}
-                            </MainButton>
-                        </div>
-
-                    )}
+            {loading ? (
+                <div className={classes.Loading}>
+                    <div className={classes.Spinner}></div>
                 </div>
-            </div>
+            ) : (
+                <>
+                    <div className={classes.ImageContainer}>
+                        <img src={selectedHero.banner} loading="lazy" alt={selectedHero.name} />
+                    </div>
+
+                    <div className={classes.ModalContent}>
+
+                        {selectedHero && Object.keys(selectedHero).length > 0 ? (
+                            <div className={classes.BackgroundContainer}>
+                                <div className={classes.TopContent}>
+                                    <header>
+                                        <div className={classes.Center}>
+                                            <h1 className={classes.Title}>
+                                                {translate('Your Progress')}
+                                            </h1>
+                                        </div>
+                                        <div className={classes.Right}>
+                                            <CloseButton timesIcon onClick={() => navigate(location.pathname)} />
+                                        </div>
+                                        <div className={classes.Center}>
+                                            <h1 className={classes.Hero}>
+                                                {translate(selectedHero.name + ' ' + selectedHero.subName)}
+                                            </h1>
+                                        </div>
+                                    </header>
+                                </div>
+                            </div>
+                        ) : (
+                            <div className={classes.BackgroundContainer}>
+                                <div className={classes.TopContent}>
+                                    <header>
+                                        <div className={classes.Center}>
+                                            <h1 className={classes.Title} style={{ width: '100%' }}>
+                                                {translate('No hero has been selected.')}
+                                            </h1>
+                                        </div>
+                                        <div className={classes.Right}>
+                                            <CloseButton timesIcon onClick={() => navigate(location.pathname)} />
+                                        </div>
+                                    </header>
+                                </div>
+                            </div>
+                        )}
+
+                        <div className={classes.MainContent}>
+                            {selectedHero && Object.keys(selectedHero).length > 0 ? (
+                                <div className={classes.GridContainer}>
+                                    {activeLevel != null &&
+                                        <section className={classes.LevelUpSection}>
+                                            <Levels activeLevel={activeLevel} onChangeLevel={(level) => setActiveLevel(level)} />
+                                            <div className={classes.LevelUpMilestone}>
+                                                <Milestones activeLevel={activeLevel} progressBar />
+                                            </div>
+                                            <Rewards />
+                                        </section>
+                                    }
+
+                                </div>
+                            ) : (
+                                <div className={classes.GoToButton}>
+                                    <MainButton color='primary' size='small' onClick={() => addParamsToUrl('heroes')}>
+                                        {translate('Select a Hero')}
+                                    </MainButton>
+                                </div>
+
+                            )}
+                        </div>
+
+
+                    </div>
+                </>
+            )}
         </div>
     );
 };
