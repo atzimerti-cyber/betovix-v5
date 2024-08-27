6db46104d24b96c64670f1f6c774eb2956eaf1be
Άννα Τζυμέρτη <atzimerti@gmail.com>
2024-08-27 18:09:24 +0300

    gg

--- src/pages/SportsMyBets/features/MyBetDetails.jsx
+++ src/pages/SportsMyBets/features/MyBetDetails.jsx
@@ -44,7 +44,7 @@ const MyBetDetails = memo(function (props) {
                 return (
                     <div key={ticketEvent.EventId}>
                         {index > 0 && <div className={classes.Separator}></div>}
-                        <div className={classes.DateOfMatch}>{formatDateTime2(ticketEvent.DateOfMatch)}</div>
+                        <div className={classes.DateOfMatch}>{ticketEvent.DateOfMatch ? formatDateTime2(ticketEvent.DateOfMatch) : null}</div>
                         <div className={classes.HeaderContainer}>
                             <div className={classes.TeamSection}>
                                 <div className={classes.EventName}>
