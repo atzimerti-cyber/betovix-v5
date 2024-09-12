f5e56b7033ab3e049805ec6ff9ec737f342ec6a5
ΠΕΤΑΣΗΣ ΠΑΝΑΓΙΩΤΗΣ <panagiotis@petasis.gr>
2024-09-12 17:50:36 +0300

    aa

--- src/features/InitApp/InitApp.jsx
+++ src/features/InitApp/InitApp.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef,useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
@@ -21,33 +21,47 @@ const InitApp = () => {
 
     const timerIdRef = useRef(null);
 
+
+    const [isLoaded, setIsLoaded] = useState (false);
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = '/src/assets/images/loading.webp';  // URL of your WebP animation
+        img.onload = () => {
+            setIsLoaded(true);  // Set the state to indicate the image is fully loaded
+        };
+    }, []);
+
     // Loads once on start
     useEffect(() => {
-       // dispatch(loadInitData(isMobile));
+        // dispatch(loadInitData(isMobile));
     }, []);
 
     // For loading initial data. Loads on change log in
     useEffect(() => {
         if (!initDataLoaded) return;
 
-        dispatch(loadInitData(isMobile));
+        //dispatch(loadInitData(isMobile));
     }, [userAccountId]);
 
     // For setting timer for getting user. Loads on change log in
     useEffect(() => {
-        // Get user every 5 seconds...
-        clearInterval(timerIdRef.current);
-        const pollingCallback = () => {
-            dispatch(getUser(navigate));
-        };
-        if (userAccountId) timerIdRef.current = setInterval(pollingCallback, 5000);
+        // // Get user every 5 seconds...
+        // clearInterval(timerIdRef.current);
+        // const pollingCallback = () => {
+        //     dispatch(getUser(navigate));
+        // };
+        // if (userAccountId) timerIdRef.current = setInterval(pollingCallback, 5000);
 
-        return () => {
-            if (!userAccountId) clearInterval(timerIdRef.current);
-        };
+        // return () => {
+        //     if (!userAccountId) clearInterval(timerIdRef.current);
+        // };
     }, [userAccountId]);
 
-    return initDataLoaded  ? <Outlet /> : <Preloader />;
+    //return initDataLoaded ? <Outlet /> : <Preloader />;
+    if( isLoaded && initDataLoaded ) return  <Outlet />;
+    if( isLoaded) return  <Preloader />;
+    return null;
 };
 
 export default InitApp;
