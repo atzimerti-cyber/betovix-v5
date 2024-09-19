36869512e7bb24aa2829236fd106202b5e99b589
Άννα Τζυμέρτη <atzimerti@gmail.com>
2024-09-19 12:09:40 +0300

    aa

--- src/features/ModalRoot/Modals/BuyLevelConfirmation.jsx
+++ src/features/ModalRoot/Modals/BuyLevelConfirmation.jsx
@@ -4,7 +4,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 
 import classes from './BuyLevelConfirmation.module.css';
 
-import WarningIcon from '../../../assets/svgs/warning.svg?react'
+import WarningIcon from '../../../assets/svgs/warning.svg?react';
 import LogoSmallIcon from '../../../assets/svgs/logo-small.svg?react';
 
 import CloseButton from '../../UI/Buttons/CloseButton';
@@ -13,7 +13,6 @@ import { translate } from '../../../utils/translations';
 import MainButton from '../../UI/Buttons/MainButton';
 import { BuyHeroLevel } from '../../../pages/UserGamification.jsx/gamificationAsyncActions';
 import { gamificationActions } from '../../../pages/UserGamification.jsx/userGamificationSlice';
-
 
 const BuyLevelConfirmationModal = () => {
     const dispatch = useDispatch();
@@ -24,82 +23,76 @@ const BuyLevelConfirmationModal = () => {
     const selectedHero = useSelector((state) => state.gamification.selectedHero);
     const lvlAction = useSelector((state) => state.gamification.displayedHero?.metadata?.lvlAction);
 
-    const [buyLevel, setbuyLevel] = useState(false);
+    const [buyLevel, setBuyLevel] = useState(false);
 
     const handleButtonClick = () => {
         const controller = new AbortController();
         const signal = controller.signal;
 
-        setPreMessage(false);
 
         dispatch(BuyHeroLevel(selectedHero, lvlAction, signal))
             .then(() => {
                 navigate(location.pathname);
             })
             .catch((error) => {
+                console.error(error);
                 navigate(location.pathname);
             });
     };
 
-    if (!selectedHero || !lvlAction) {
-        navigate(location.pathname);
-        return null
-    }
-
     return (
         <div className={classes.ConfirmationModal}>
-            {buyLevel ?
-                (
-                    <div className={classes.ModalContent}>
-                        <div className={classes.BackgroundContainer}>
-                            <div className={classes.TopContent}>
-                                <header>
-                                    <div className={classes.Center}>
-                                        <h1 className={classes.Title}>
-                                            <WarningIcon className={classes.WarningIcon} />
-                                            {translate('Warning!')}
-                                        </h1>
-                                    </div>
-                                    <div className={classes.Right}>
-                                        <CloseButton timesIcon onClick={() => navigate(location.pathname)} />
-                                    </div>
-                                </header>
-                            </div>
-                        </div>
-                        <div className={classes.MainContent}>
-                            <p className={classes.Message}>
-                                {translate(`You picked to buy level ${lvlAction} of ${selectedHero?.name}.`)}<br></br>
-                                {translate('Once you buy a level, you cannot go back! Are you sure you want to buy this level?')}
-                            </p>
-                        </div>
-                        <div className={classes.Buttons}>
-                            <MainButton color='bv-light-green' onClick={handleButtonClick}>
-                                <span>{translate('YES, I am sure')}</span>
-                            </MainButton>
-                            <MainButton color='dark' onClick={() => navigate(location.pathname)}>
-                                <span>{translate('NO, go back')}</span>
-                            </MainButton>
+            {buyLevel ? (
+                <div className={classes.ModalContent}>
+                    <div className={classes.BackgroundContainer}>
+                        <div className={classes.TopContent}>
+                            <header>
+                                <div className={classes.Center}>
+                                    <h1 className={classes.Title}>
+                                        <WarningIcon className={classes.WarningIcon} />
+                                        {translate('Warning!')}
+                                    </h1>
+                                </div>
+                                <div className={classes.Right}>
+                                    <CloseButton timesIcon onClick={() => navigate(location.pathname)} />
+                                </div>
+                            </header>
                         </div>
                     </div>
-                ) : (
-                    <div className={classes.ModalContent} >
-                        <div className={classes.BackgroundContainer}>
-                            <div className={classes.TopContent}>
-                                <header>
-                                    <div className={classes.Center}>
-                                        <h1 className={classes.Title} style={{ fontSize: "0.9rem", fontWeight: "500" }}>
-                                            <LogoSmallIcon className={classes.WarningIcon} style={{ width: '27px', height: '27px' }} />
-                                            {translate('Coming Soon!')}
-                                        </h1>
-                                    </div>
-                                    <div className={classes.Right}>
-                                        <CloseButton timesIcon onClick={() => navigate(location.pathname)} />
-                                    </div>
-                                </header>
-                            </div>
+                    <div className={classes.MainContent}>
+                        <p className={classes.Message}>
+                            {translate(`You picked to buy level ${lvlAction} of ${selectedHero?.name}.`)}<br />
+                            {translate('Once you buy a level, you cannot go back! Are you sure you want to buy this level?')}
+                        </p>
+                    </div>
+                    <div className={classes.Buttons}>
+                        <MainButton color="bv-light-green" onClick={handleButtonClick}>
+                            <span>{translate('YES, I am sure')}</span>
+                        </MainButton>
+                        <MainButton color="dark" onClick={() => navigate(location.pathname)}>
+                            <span>{translate('NO, go back')}</span>
+                        </MainButton>
+                    </div>
+                </div>
+            ) : (
+                <div className={classes.ModalContent}>
+                    <div className={classes.BackgroundContainer}>
+                        <div className={classes.TopContent}>
+                            <header>
+                                <div className={classes.Center}>
+                                    <h1 className={classes.Title} style={{ fontSize: '0.9rem', fontWeight: 500 }}>
+                                        <LogoSmallIcon className={classes.WarningIcon} style={{ width: '27px', height: '27px' }} />
+                                        {translate('Coming Soon!')}
+                                    </h1>
+                                </div>
+                                <div className={classes.Right}>
+                                    <CloseButton timesIcon onClick={() => navigate(location.pathname)} />
+                                </div>
+                            </header>
                         </div>
                     </div>
-                )}
+                </div>
+            )}
         </div>
     );
 };
