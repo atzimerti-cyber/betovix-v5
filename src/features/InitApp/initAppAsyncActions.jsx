3e41221437b751db4828a360e128494a335d02df
@kristireci01 <kristireci01@gmail.com>
2025-03-26 17:56:26 +0200

    aa

--- src/features/InitApp/initAppAsyncActions.jsx
+++ src/features/InitApp/initAppAsyncActions.jsx
@@ -790,7 +790,7 @@ export const getSite = (signal) => {
       const link = document.createElement("link");
       link.rel = "stylesheet";
       link.type = "text/css";
-      //link.href = "/themes/theme-9.css";
+      //link.href = "/themes/theme-12.css";
       link.href = response.data.Contents.SiteTheme; ////////////////////
       document.head.appendChild(link);
 
