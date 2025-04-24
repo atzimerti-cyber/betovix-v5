ea949f4e87f5a2352a073177566d1a37aa17b3c2
@kristireci01 <kristireci01@gmail.com>
2025-04-24 15:23:54 +0300

    ww

--- src/pages/SportsMyBets/features/MyBetDetails.jsx
+++ src/pages/SportsMyBets/features/MyBetDetails.jsx
@@ -1,114 +1,144 @@
-import { memo } from 'react';
-import { useSelector } from 'react-redux';
+import { memo } from "react";
+import { useSelector } from "react-redux";
 
-import classes from './MyBetDetails.module.css';
+import classes from "./MyBetDetails.module.css";
 // import TeamLogo from '../../../features/TeamLogo/TeamLogo';
-import { translate } from '../../../utils/translations';
-import { addThousandsSeparator, formatDateTime2, formatTimeString } from '../../../utils/custom';
-import LiveBadge from '../../../features/LiveBadge/LiveBadge';
+import { translate } from "../../../utils/translations";
+import {
+  addThousandsSeparator,
+  formatDateTime2,
+  formatTimeString,
+} from "../../../utils/custom";
+import LiveBadge from "../../../features/LiveBadge/LiveBadge";
 
 const MyBetDetails = memo(function (props) {
-    const lang = useSelector((state) => state.app.lang); // Necessary for rerendering translations
-    const liveState = useSelector((state) => state.live.liveState);
+  const lang = useSelector((state) => state.app.lang); // Necessary for rerendering translations
+  const liveState = useSelector((state) => state.live.liveState);
 
-    const getStatusText = (event) => {
-        let text = event.Header.MatchTimeExtended;
+  const getStatusText = (event) => {
+    let text = event.Header.MatchTimeExtended;
 
-        if (event?.Header.MatchTimeExtended !== 'Not Started') {
-            if (event?.Header?.MatchTime) text = event?.Header.MatchTime + '′ - ' + text;
-            else if (event?.Header?.RemainingTimeInPeriod && event?.Header?.RemainingTimeInPeriod !== '')
-                text = formatTimeString(event?.Header.RemainingTimeInPeriod) + ' - ' + text;
-        }
+    if (event?.Header.MatchTimeExtended !== "Not Started") {
+      if (event?.Header?.MatchTime)
+        text = event?.Header.MatchTime + "′ - " + text;
+      else if (
+        event?.Header?.RemainingTimeInPeriod &&
+        event?.Header?.RemainingTimeInPeriod !== ""
+      )
+        text =
+          formatTimeString(event?.Header.RemainingTimeInPeriod) + " - " + text;
+    }
 
-        return text;
-    };
+    return text;
+  };
 
-    const getNotLiveGameScore = (ticketEvent) => {
-        if (ticketEvent.Tournament.includes('Outright') || ticketEvent.Tournament.includes('Specials')) {
-            return '';
-        }
+  const getNotLiveGameScore = (ticketEvent) => {
+    if (
+      ticketEvent.Tournament.includes("Outright") ||
+      ticketEvent.Tournament.includes("Specials")
+    ) {
+      return "";
+    }
 
-        const result = JSON.parse(ticketEvent.Result);
+    const result =
+      ticketEvent?.Result &&
+      ticketEvent?.Result !== "-" &&
+      JSON.parse(ticketEvent?.Result);
 
-        if (result) {
-            const score = result.away !== null ? result.home + '-' + result.away : result.home;
-            return score;
-        }
+    if (result) {
+      const score =
+        result.away !== null ? result.home + "-" + result.away : result.home;
+      return score;
+    }
 
-        return '';
-    };
+    return "";
+  };
 
-    return (
-        <section className={classes.MyBetDetailsSection}>
-            {props.item.TicketEvents.map((ticketEvent, index) => {
-                return (
-                    <div key={ticketEvent?.TicketEventId}>
-                        {index > 0 && <div className={classes.Separator}></div>}
-                        <div className={classes.DateOfMatch}>
-                            {ticketEvent.DateOfMatch ? (
-                                formatDateTime2(ticketEvent.DateOfMatch)
-                            ) : (
-                                ticketEvent.Live && <LiveBadge />
-                            )}
-                        </div>
-                        <div className={classes.HeaderContainer}>
-                            <div className={classes.TeamSection}>
-                                <div className={classes.EventName}>
-                                    <div className={classes.TeamVersusGroup}>{ticketEvent.Event}</div>
-                                </div>
+  return (
+    <section className={classes.MyBetDetailsSection}>
+      {props.item.TicketEvents.map((ticketEvent, index) => {
+        return (
+          <div key={ticketEvent?.TicketEventId}>
+            {index > 0 && <div className={classes.Separator}></div>}
+            <div className={classes.DateOfMatch}>
+              {ticketEvent.DateOfMatch
+                ? formatDateTime2(ticketEvent.DateOfMatch)
+                : ticketEvent.Live && <LiveBadge />}
+            </div>
+            <div className={classes.HeaderContainer}>
+              <div className={classes.TeamSection}>
+                <div className={classes.EventName}>
+                  <div className={classes.TeamVersusGroup}>
+                    {ticketEvent.Event}
+                  </div>
+                </div>
 
-                                <div className={classes.OutcomeContainer}>
-                                    <div className={classes.Outcome}>
-                                        <span className={classes.MarketName}>{translate(ticketEvent.MarketName)}</span>
-                                        <span className={classes.SectionOutcome}>{ticketEvent.PointName}</span>
-                                    </div>
-                                </div>
-                            </div>
+                <div className={classes.OutcomeContainer}>
+                  <div className={classes.Outcome}>
+                    <span className={classes.MarketName}>
+                      {translate(ticketEvent.MarketName)}
+                    </span>
+                    <span className={classes.SectionOutcome}>
+                      {ticketEvent.PointName}
+                    </span>
+                  </div>
+                </div>
+              </div>
 
-                            <div className={classes.OddsSection}>
-                                <div
-                                    className={
-                                        ticketEvent.Wins === 'W'
-                                            ? [classes.StatusContainer, classes.Win].join(' ')
-                                            : ticketEvent.Wins === 'L'
-                                            ? [classes.StatusContainer, classes.Loss].join(' ')
-                                            : classes.StatusContainer
-                                    }
-                                >
-                                    <div className={classes.Circle}></div>
-                                    <span className={classes.Status}>
-                                        {ticketEvent.Wins === 'W' ? translate('Win') : ticketEvent.Wins === 'L' ? translate('Loss') : translate('Pending')}
-                                    </span>
-                                </div>
-                                <div className={classes.Odds}>
-                                    <span>{addThousandsSeparator(ticketEvent.Odd)}</span>
-                                </div>
-                            </div>
-                        </div>
+              <div className={classes.OddsSection}>
+                <div
+                  className={
+                    ticketEvent.Wins === "W"
+                      ? [classes.StatusContainer, classes.Win].join(" ")
+                      : ticketEvent.Wins === "L"
+                      ? [classes.StatusContainer, classes.Loss].join(" ")
+                      : classes.StatusContainer
+                  }
+                >
+                  <div className={classes.Circle}></div>
+                  <span className={classes.Status}>
+                    {ticketEvent.Wins === "W"
+                      ? translate("Win")
+                      : ticketEvent.Wins === "L"
+                      ? translate("Loss")
+                      : translate("Pending")}
+                  </span>
+                </div>
+                <div className={classes.Odds}>
+                  <span>{addThousandsSeparator(ticketEvent.Odd)}</span>
+                </div>
+              </div>
+            </div>
 
-                        {liveState[ticketEvent?.TicketEventId] ? (
-                            <div className={classes.InfoContainer}>
-                                <div className={classes.EventTime}>
-                                    {liveState[ticketEvent.EventId] && (
-                                        <>
-                                            <LiveBadge />
-                                            <div className={classes.Time}>{getStatusText(liveState[ticketEvent.EventId])}</div>
-                                        </>
-                                    )}
-                                </div>
-                                <div className={classes.InfoScore}>{liveState[ticketEvent.EventId].Header?.Score}</div>
-                            </div>
-                        ) : (
-                            <div className={classes.InfoContainer}>
-                                <div className={classes.EventTime}></div>
-                                <div className={classes.InfoScore}>{getNotLiveGameScore(ticketEvent)}</div>
-                            </div>
-                        )}
-                    </div>
-                );
-            })}
-        </section>
-    );
+            {liveState[ticketEvent?.TicketEventId] ? (
+              <div className={classes.InfoContainer}>
+                <div className={classes.EventTime}>
+                  {liveState[ticketEvent.EventId] && (
+                    <>
+                      <LiveBadge />
+                      <div className={classes.Time}>
+                        {getStatusText(liveState[ticketEvent.EventId])}
+                      </div>
+                    </>
+                  )}
+                </div>
+                <div className={classes.InfoScore}>
+                  {liveState[ticketEvent.EventId].Header?.Score}
+                </div>
+              </div>
+            ) : (
+              <div className={classes.InfoContainer}>
+                <div className={classes.EventTime}></div>
+                <div className={classes.InfoScore}>
+                  {getNotLiveGameScore(ticketEvent)}
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </section>
+  );
 });
 
 export default MyBetDetails;
