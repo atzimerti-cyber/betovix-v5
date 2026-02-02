50b9cc7596c533f6d5e46ca3b0f97d1adb05b527
@kristireci01 <kristireci01@gmail.com>
2026-02-02 16:01:13 +0200

    FIXXXXX

--- public/settings.js
+++ public/settings.js
@@ -21,23 +21,23 @@ window.SETTINGS = {
     CASINO_OPEN_STYLE: 'WITHBONUS' //WITHBONUS - FULLSCREEN
 }
 
-window.SETTINGS = {
-    VITE_CASINO_BASE: 'https://casinoapi.modulesports.com:61020/api',
-    VITE_WALLET_API_BASE: 'https://modulesports.com:61009/api',
-    VITE_SPORTS_API_BASE: 'https://modulesports.com:60009/api',
-    VITE_SPORTS_LOGOS: 'https://cdnsports.modulesports.com/assets',
-    VITE_CASINO_LOBBY: 'https://casinom.storetube.gr',
-    VITE_CASINO_LIVE_LOBBY: 'https://casinom.storetube.gr',
-    VITE_BETS_API: 'https://modulesports.com:60019/api',
-    VITE_SITE_ID: 0,
-    VITE_HOME_URL: 'https://betovix.storetube.gr',
-    VITE_VEGAS_HOME_URL: 'https://betovix.com',
+// window.SETTINGS = {
+//     VITE_CASINO_BASE: 'https://casinoapi.modulesports.com:61020/api',
+//     VITE_WALLET_API_BASE: 'https://modulesports.com:61009/api',
+//     VITE_SPORTS_API_BASE: 'https://modulesports.com:60009/api',
+//     VITE_SPORTS_LOGOS: 'https://cdnsports.modulesports.com/assets',
+//     VITE_CASINO_LOBBY: 'https://casinom.storetube.gr',
+//     VITE_CASINO_LIVE_LOBBY: 'https://casinom.storetube.gr',
+//     VITE_BETS_API: 'https://modulesports.com:60019/api',
+//     VITE_SITE_ID: 0,
+//     VITE_HOME_URL: 'https://betovix.storetube.gr',
+//     VITE_VEGAS_HOME_URL: 'https://betovix.com',
 
-    VITE_WALLET_STORETUBE: 'https://modulesports.com:61009/api',
-    VITE_STORETUBE: 'https://modulesports.com:60019/api',
-    VITE_CASINO_STORETUBE_BASE: 'https://casinoapi.storetube.gr:61020/api',
+//     VITE_WALLET_STORETUBE: 'https://modulesports.com:61009/api',
+//     VITE_STORETUBE: 'https://modulesports.com:60019/api',
+//     VITE_CASINO_STORETUBE_BASE: 'https://casinoapi.storetube.gr:61020/api',
 
-    VITE_GAMIFICATION_STORETUBE: 'https://gamificationapi.storetube.gr:61113/api',
-    VITE_UPLOAD: 'https://wallet1.modulesports.com:61009/api',
-    CASINO_OPEN_STYLE: 'FULLSCREEN' //WITHBONUS - FULLSCREEN
-} 
+//     VITE_GAMIFICATION_STORETUBE: 'https://gamificationapi.storetube.gr:61113/api',
+//     VITE_UPLOAD: 'https://wallet1.modulesports.com:61009/api',
+//     CASINO_OPEN_STYLE: 'FULLSCREEN' //WITHBONUS - FULLSCREEN
+// } 
